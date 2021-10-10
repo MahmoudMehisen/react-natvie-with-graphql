08bb2380ce06044e631c26e60a1f1ac4a49054f3
MahmoudMehisen <mahmoud.mehisen@gmail.com>
2021-10-10 17:58:37 +0200

    first

---
+++ App.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+
+import { ApolloProvider, Query } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+const client = new ApolloClient({ uri: 'http://192.168.1.13:4000/graphql' })
+import Pokemon from "./src/components/Pokemon";
+import getRandomInt from "./src/helpers/getRandomInt";
+export const AppContext = React.createContext({ data: { pokemon: null } });
+
+
+export default class App extends Component {
+
+  state = {
+    query: null
+  }
+
+
+  componentDidMount() {
+    const query = this.getQuery();
+    this.setState({
+      query
+    });
+  }
+
+
+  getQuery = () => {
+    const randomID = getRandomInt(1, 807);
+   let queryString =  `
+    query GetPokemonById {
+      pokemon(id: ${randomID}) {
+        id,
+        name,
+        desc,
+        pic,
+        types {
+          id,
+          name
+        }
+      }
+    }
+  `
+
+    return queryString;
+  }
+
+
+  render() {
+    const { query } = this.state;
+    if (!query) return null;
+
+    return (
+      <ApolloProvider client={client}>
+        <Query query={gql`${query}`} >
+          {({ loading, error, data }) => {
+            if (loading || error) return <ActivityIndicator size="large" color="#0000ff" />
+
+            return (
+              <AppContext.Provider value={{...data.pokemon, onPress: this.onGetNewPokemon}} style={styles.container}>
+                <Pokemon />
+                
+              </AppContext.Provider>
+            )
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
+  //
+
+  onGetNewPokemon = () => {
+    const query = this.getQuery();
+    this.setState({
+      query
+    });
+  }
+
+  
+
+}
+//
+
+const styles = {
+  container: {
+    flex: 1
+  }
+};
